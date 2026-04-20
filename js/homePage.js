9bb77ff049405f58b177939e38c101c37bdca82f
vishmi <>
2026-04-20 16:13:37 +0800

    edit home js

--- js/homePage.js
+++ js/homePage.js
@@ -5,12 +5,13 @@ document.addEventListener("DOMContentLoaded", () => {
     "../images/homePic3.jpg",
     "../images/homepic4.jpg",
   ];
-
   const randomImage = images[Math.floor(Math.random() * images.length)];
-
   const heroBg = document.getElementById("hero-bg");
+
+
 
   if (heroBg) {
     heroBg.style.backgroundImage = `url('${randomImage}')`;
   }
+  
 });
